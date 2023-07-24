fd87e7631a3afb971f88f17d093c12310ed1f809
patrykos9 <patrykoffer30@gmail.com>
2023-07-24 19:51:25 +0200

    Start commit

---
+++ src/main/java/com/shop/hotshop/HotshopApplication.java
@@ -0,0 +1,13 @@
+package com.shop.hotshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/hotshop/HotshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.hotshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
